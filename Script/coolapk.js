c48ff5ca1b130f4f7e93e6665a6f02e700ae90fa
Z <155460219+z-jinke@users.noreply.github.com>
2024-12-05 21:53:59 +0800

    Update coolapk.js

--- Script/coolapk.js
+++ Script/coolapk.js
@@ -1,12 +1,13 @@
 let url = $request.url;
 let body = $response.body;
 
+// 初始化配置
 if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
         obj.data.forEach(item => {
             if (item.extraDataArr) {
-                const adKeysToModify = [
+                const adKeysToModify = new Set([
                     "Ad.CHANGE_AFTER_SHOW",
                     "Ad.ADLOAD_POS_ID",
                     "Ad.KS_APP_ID",
@@ -23,7 +24,7 @@ if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
                     "Ad.SPLASH_RETRY_PERIOD",
                     "Ad.CLICK_BUTTON_AREA",
                     "Ad.BZ_APP_ID"
-                ];
+                ]);
                 adKeysToModify.forEach(key => {
                     if (key in item.extraDataArr) {
                         item.extraDataArr[key] = "0";
@@ -32,52 +33,53 @@ if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
                 item.extraDataArr["SplashAd.timeout"] = 0;
                 item.extraDataArr["SplashAd.Expires"] = 9999999999;
             }
-            const filterEntityIds = [1681, 1633, 1710, 1754, 1966, 1229, 413, 417, 845, 2258, 1170, 2018];
+            const filterEntityIds = new Set([1681, 1633, 1710, 1754, 1966, 1229, 413, 417, 845, 2258, 1170, 2018]);
             if (item.entities) {
-                item.entities = item.entities.filter(entity => entity && !filterEntityIds.includes(entity.entityId));
+                item.entities = item.entities.filter(entity => entity && !filterEntityIds.has(entity.entityId));
             }
         });
     }
     body = JSON.stringify(obj);
-    $done({ body });
 }
 
-else if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/indexV8/.test(url)) {
+// 首页精简
+if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/indexV8/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        const filterEntityIds = [32557, 13635, 29349];
-        obj.data = obj.data.filter(item => !filterEntityIds.includes(item.entityId));
+        const filterEntityIds = new Set([32557, 13635, 29349]);
+        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
     }
     body = JSON.stringify(obj);
-    $done({ body });
 }
 
-else if (/^https?:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
+// 每个Tab模块
+if (/^https?:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        const filterEntityIds = [20099, 29213, 36278, 36279, 14379, 24309, 12889, 40133, 39940, 37755];
-        obj.data = obj.data.filter(item => !filterEntityIds.includes(item.entityId));
+        const filterEntityIds = new Set([20099, 29213, 36278, 36279, 14379, 24309, 12889, 40133, 39940, 37755]);
+        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
     }
     body = JSON.stringify(obj);
-    $done({ body });
 }
 
-else if (/^https?:\/\/api\.coolapk\.com\/v6\/search/.test(url)) {
+// 搜索栏
+if (/^https?:\/\/api\.coolapk\.com\/v6\/search/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        const filterEntityIds = [20252, 16977];
-        obj.data = obj.data.filter(item => !filterEntityIds.includes(item.entityId));
+        const filterEntityIds = new Set([20252, 16977]);
+        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
     }
     body = JSON.stringify(obj);
-    $done({ body });
 }
 
-else if (/^https?:\/\/api\.coolapk\.com\/v6\/account\/loadConfig/.test(url)) {
+// 我的页面
+if (/^https?:\/\/api\.coolapk\.com\/v6\/account\/loadConfig/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        const filterEntityIds = [14809, 1002, 1005];
-        obj.data = obj.data.filter(item => !filterEntityIds.includes(item.entityId));
+        const filterEntityIds = new Set([14809, 1002, 1005]);
+        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
     }
     body = JSON.stringify(obj);
-    $done({ body });
 }
+
+$done({ body });
