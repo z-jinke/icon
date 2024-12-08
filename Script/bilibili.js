65fcbcde479b0b584ad0fc45803b30ee0196db66
Z <155460219+z-jinke@users.noreply.github.com>
2024-12-08 23:24:12 +0800

    Update bilibili.js

--- Script/bilibili.js
+++ Script/bilibili.js
@@ -32,7 +32,6 @@ if (/^https?:\/\/app\.bilibili\.com\/x\/resource\/show\/tab/.test(url)) {
         ];
         obj.data.bottom = [
             { id: 43, name: "首页", uri: "bilibili://main/home/", tab_id: "home", icon: "http://i0.hdslb.com/bfs/archive/1ab5459ccb18c7a996315327257375be3da19886.png", icon_selected: "http://i0.hdslb.com/bfs/archive/d6a45f06684562dd9cb6914007658c0cdb17bbff.png", pos: 1 },
-            { id: 44, name: "频道", uri: "bilibili://pegasus/channel/", tab_id: "频道Bottom", icon: "http://i0.hdslb.com/bfs/archive/b4f621f268c1f9eda501805135f132aa9498b0ba.png", icon_selected: "http://i0.hdslb.com/bfs/archive/94539249e59621214f7dc1226cf38a2b8fe4c64f.png", pos: 2 },
             { id: 45, name: "动态", uri: "bilibili://following/home/", tab_id: "dynamic", icon: "http://i0.hdslb.com/bfs/archive/0f15d5f5be25af29eec6f002561d5000a77cc914.png", icon_selected: "http://i0.hdslb.com/bfs/archive/1d37925562cd3e7d2e5f0868f966b5b9a8b86cde.png", pos: 3 },
             { id: 49, name: "我的", uri: "bilibili://user_center/", tab_id: "我的Bottom", icon: "http://i0.hdslb.com/bfs/archive/aafe71f10eeb5086ac119e4dad769c5aad4d86a2.png", icon_selected: "http://i0.hdslb.com/bfs/archive/36e080bbd8ae858af664ef251741124e04241942.png", pos: 5 }
         ];
