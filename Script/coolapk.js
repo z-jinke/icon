9ff34834203e87b9f79e19d43fdfa3305dc09072
Z <155460219+z-jinke@users.noreply.github.com>
2024-12-11 17:27:24 +0800

    Update coolapk.js

--- Script/coolapk.js
+++ Script/coolapk.js
@@ -6,16 +6,16 @@ if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     if (obj.data) {
         obj.data.forEach(item => {
             if (item.extraDataArr) {
-                const adKeysToModify = new Set(["Ad.CHANGE_AFTER_SHOW", "Ad.ADLOAD_POS_ID", "Ad.KS_APP_ID", "Ad.GDT_APP_ID", "Ad.TO_APP_KEY", "Ad.PRELOAD_THRESHOLD_IOS", "Ad.DOWNLOAD_POPUP", "Ad.GROWTH_BACK_PRESS", "Ad.PRELOAD", "Ad.PRELOAD_AFTER_USE", "Ad.TO_APP_ID", "Ad.PRELOAD_THRESHOLD", "Ad.SPLASH_DOWNLOAD_POPUP", "Ad.SPLASH_RETRY_PERIOD", "Ad.CLICK_BUTTON_AREA", "Ad.BZ_APP_ID"]);
+                const adKeysToModify = new Set(["Ad.CHANGE_AFTER_SHOW","Ad.ADLOAD_POS_ID","Ad.KS_APP_ID","Ad.DOWNLOAD_POPUP","Ad.PRELOAD","Ad.PRELOAD_AFTER_USE","Ad.PRELOAD_THRESHOLD","Ad.PRELOAD_THRESHOLD_IOS","Ad.GDT_APP_ID","Ad.TO_APP_ID","Ad.TO_APP_KEY","Ad.TT_APP_ID","Ad.BZ_APP_ID","Ad.SPLASH_DOWNLOAD_POPUP","Ad.SPLASH_RETRY_PERIOD","Ad.CLICK_BUTTON_AREA","Ad.GROWTH_BACK_PRESS"]);
                 adKeysToModify.forEach(key => {
-                    if (key in item.extraDataArr) {
-                        item.extraDataArr[key] = "0";
+            if (key in item.extraDataArr) {
+                       item.extraDataArr[key] = "";
                     }
                 });
                 item.extraDataArr["SplashAd.timeout"] = 0;
                 item.extraDataArr["SplashAd.Expires"] = 9999999999;
             }
-            const filterEntityIds = new Set([1681, 1633, 1710, 1754, 1966, 1229, 413, 417, 845, 2258, 1170, 2018, 2274]);
+            const filterEntityIds = new Set([1681, 1633, 1710, 1754, 1966, 1229, 413, 417, 845, 2258, 1170, 2018, 2274, 944]);
             if (item.entities) {
                 item.entities = item.entities.filter(entity => entity && !filterEntityIds.has(entity.entityId));
             }
