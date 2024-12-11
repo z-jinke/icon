874a443fb5a82af261e58e276c069bca6de13181
Z <155460219+z-jinke@users.noreply.github.com>
2024-12-11 17:35:30 +0800

    Update coolapk.js

--- Script/coolapk.js
+++ Script/coolapk.js
@@ -6,10 +6,31 @@ if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     if (obj.data) {
         obj.data.forEach(item => {
             if (item.extraDataArr) {
-                const adKeysToModify = new Set(["Ad.CHANGE_AFTER_SHOW","Ad.ADLOAD_POS_ID","Ad.KS_APP_ID","Ad.DOWNLOAD_POPUP","Ad.PRELOAD","Ad.PRELOAD_AFTER_USE","Ad.PRELOAD_THRESHOLD","Ad.PRELOAD_THRESHOLD_IOS","Ad.GDT_APP_ID","Ad.TO_APP_ID","Ad.TO_APP_KEY","Ad.TT_APP_ID","Ad.BZ_APP_ID","Ad.SPLASH_DOWNLOAD_POPUP","Ad.SPLASH_RETRY_PERIOD","Ad.CLICK_BUTTON_AREA","Ad.GROWTH_BACK_PRESS"]);
+                const adKeysToModify = new Set([
+                    "Ad.CHANGE_AFTER_SHOW",
+                    "Ad.ADLOAD_POS_ID",
+                    "Ad.KS_APP_ID",
+                    "Ad.DOWNLOAD_POPUP",
+                    "Ad.PRELOAD",
+                    "Ad.PRELOAD_AFTER_USE",
+                    "Ad.PRELOAD_THRESHOLD",
+                    "Ad.PRELOAD_THRESHOLD_IOS",
+                    "Ad.GDT_APP_ID",
+                    "Ad.TO_APP_ID",
+                    "Ad.TO_APP_KEY",
+                    "Ad.TT_APP_ID",
+                    "Ad.BZ_APP_ID",
+                    "Ad.SPLASH_DOWNLOAD_POPUP",
+                    "Ad.SPLASH_RETRY_PERIOD",
+                    "Ad.CLICK_BUTTON_AREA",
+                    "Ad.GROWTH_BACK_PRESS",
+                    "MainActivity.redPacketIconV2",
+                    "MainActivity.redPacketFloat",
+                    "MainActivity.redPacketFloatUrl"
+                ]);
                 adKeysToModify.forEach(key => {
-            if (key in item.extraDataArr) {
-                       item.extraDataArr[key] = "";
+                    if (key in item.extraDataArr) {
+                        item.extraDataArr[key] = "";
                     }
                 });
                 item.extraDataArr["SplashAd.timeout"] = 0;
