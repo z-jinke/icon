46a2ae8d724e026c2d48ddc94b6b8846aa839475
Z <155460219+z-jinke@users.noreply.github.com>
2024-12-12 16:08:22 +0800

    Update coolapk.js

--- Script/coolapk.js
+++ Script/coolapk.js
@@ -14,7 +14,7 @@ if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
                     }
                 }
             }     
-            const filterEntityIds = new Set([1681, 1633, 1710, 1754, 1966, 1229, 2261, 417, 845, 2258, 1170, 2018, 2274, 944]);
+            const filterEntityIds = new Set([1681, 1633, 1710, 1754, 1966, 2261, 413, 1229, 417, 845, 2258, 1170, 2018, 2274, 944]);
             if (item.entities) {
                 item.entities = item.entities.filter(entity => entity && !filterEntityIds.has(entity.entityId));
             }
@@ -27,8 +27,7 @@ if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
 if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/indexV8/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        const filterEntityIds = new Set([32557, 13635, 29349, 29519]);
-        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
+        obj.data = obj.data.filter(item => item.entityType === "feed");
     }
     body = JSON.stringify(obj);
     $done({ body });
@@ -36,9 +35,8 @@ if (/^https?:\/\/api\.coolapk\.com\/v6\/main\/indexV8/.test(url)) {
 
 if (/^https?:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
     let obj = JSON.parse(body);
-    if (obj.data) {
-        const filterEntityIds = new Set([20099, 29213, 36278, 36279, 14379, 24309, 12889, 40133, 39940, 37755]);
-        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
+    if (obj.data) { 
+        obj.data = obj.data.filter(item => item.entityId!="24309" && item.entityId!="12889" && item.entityId!="36279"  && item.entityId!="36278" && item.entityId!="40133" && item.entityId!="37755" &&  item.entityId!="29213");
     }
     body = JSON.stringify(obj);
     $done({ body });
@@ -47,8 +45,7 @@ if (/^https?:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
 if (/^https?:\/\/api\.coolapk\.com\/v6\/account\/loadConfig/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        const filterEntityIds = new Set([14809, 1002, 1005]);
-        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
+        obj.data = obj.data.filter(item => item.entityId!="14809" && item.entityId!="1002" && item.entityId!="1005");
     }
     body = JSON.stringify(obj);
     $done({ body });
