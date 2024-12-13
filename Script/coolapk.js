c3f72d8717e468dd6038861cf492d59267ad26bd
Z <155460219+z-jinke@users.noreply.github.com>
2024-12-14 02:10:01 +0800

    Update coolapk.js

--- Script/coolapk.js
+++ Script/coolapk.js
@@ -4,6 +4,7 @@ let body = $response.body;
 if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
+        const filterEntityIds = new Set([1681, 1633, 1710, 1754, 1966, 2261, 413, 1229, 417, 845, 2258, 1170, 2018, 2274, 944]);
         obj.data.forEach(item => {
             if (item.extraDataArr) {
                 for (let key in item.extraDataArr) {
@@ -12,7 +13,6 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(url)) {
                     }
                 }
             }
-            const filterEntityIds = new Set([1681, 1633, 1710, 1754, 1966, 2261, 413, 1229, 417, 845, 2258, 1170, 2018, 2274, 944]);
             if (item.entities) {
                 item.entities = item.entities.filter(entity => entity && !filterEntityIds.has(entity.entityId));
             }
@@ -44,9 +44,8 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/feed\/detail\?id/.test(url)) {
 if (/^https:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        obj.data = obj.data.filter(item =>
-            !["24309", "12889", "36279", "36278", "40133", "37755", "29213"].includes(item.entityId)
-        );
+        const filterEntityIds = new Set([24309, 12889, 29176, 36279, 36278, 29213, 29178, 37755, 40133]);
+        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
     }
     body = JSON.stringify(obj);
     $done({ body });
@@ -55,9 +54,8 @@ if (/^https:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(url)) {
 if (/^https:\/\/api\.coolapk\.com\/v6\/account\/loadConfig/.test(url)) {
     let obj = JSON.parse(body);
     if (obj.data) {
-        obj.data = obj.data.filter(item =>
-            !["14809", "1002", "1005"].includes(item.entityId)
-        );
+        const filterEntityIds = new Set([14809, 1002]);
+        obj.data = obj.data.filter(item => !filterEntityIds.has(item.entityId));
     }
     body = JSON.stringify(obj);
     $done({ body });
